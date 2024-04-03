fa837b1bc788b47407d45b1313a6b0f1baa995ef
Yurii Kovalchuk <urakovalchuk3535@gmail.com>
2024-04-03 11:43:46 +0300

    additional: 1365

---
+++ floyd-dijkstra/additional/1365.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+int dijkstra(const std::vector<std::vector<std::pair<int, int>>>& arr, int n, int s, int f) {
+    std::vector<int> weight(n + 1);
+    std::unordered_set<int> not_processed;
+    for (int i = 1; i <= n; ++i)
+        not_processed.insert(i);
+    std::fill(weight.begin(), weight.end(), -1);
+    weight[s] = 0;
+    int curr_v, next_v, edge;
+
+    while (!not_processed.empty()) {
+        curr_v = *std::min_element(not_processed.begin(), not_processed.end(), [&](int l, int r)
+            { return weight[l] != -1 && (weight[r] == -1 || weight[l] < weight[r]); });
+        if (weight[curr_v] == -1)
+            break;
+        not_processed.erase(curr_v);
+        for (const auto& x : arr[curr_v]) {
+            next_v = x.first;
+            edge = x.second;
+            if (weight[next_v] == -1 || weight[curr_v] + edge < weight[next_v])
+                weight[next_v] = weight[curr_v] + edge;
+        }
+    }
+    return weight[f];
+}
+
+
+int main() {
+    int n, m, s, f, w;
+    std::cin >> n >> s >> f;
+    std::vector<std::vector<std::pair<int, int>>> arr(n + 1);
+
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= n; ++j) {
+            std::cin >> w;
+            if (i != j && w != -1)
+                arr[i].push_back(std::make_pair(j, w));
+        }
+
+    std::cout << dijkstra(arr, n, s, f);
+
+    return 0;
+}
