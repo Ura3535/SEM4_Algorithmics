fcfba9d74a0e070a9e5d855fb62e8da0717994a3
Ura3535 <127392046+Ura3535@users.noreply.github.com>
2024-04-16 10:06:50 +0300

    Update 0065.py

--- combinatorics/HW/0065.py
+++ combinatorics/HW/0065.py
@@ -5,9 +5,9 @@ def comb(n, k) :
         res = res * (n - k + i) // i
         return res
 
-        n, s = map(int, input().split())
-        res = 0
-        for i in range(n // 2):
-            res += comb(s - 2 + i, s - 2)
-            res = comb(n + s - 1, s - 1) - s * res
-            print(res)
+n, s = map(int, input().split())
+res = 0
+for i in range(n // 2):
+    res += comb(s - 2 + i, s - 2)
+    res = comb(n + s - 1, s - 1) - s * res
+    print(res)
